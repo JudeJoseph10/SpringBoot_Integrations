2f7e6bc834879ba03f437e23a2ae52661baba38c
judearun.joseph <judearun.joseph@gmail.com>
2026-06-28 10:37:15 +0530

    spring boot recaptcha integration

---
+++ springboot-recaptcha/src/main/java/dev/danvega/authdemo/Application.java
@@ -0,0 +1,23 @@
+package dev.danvega.authdemo;
+
+import dev.danvega.authdemo.security.RecaptchaProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableConfigurationProperties(RecaptchaProperties.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+}
